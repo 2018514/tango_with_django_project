1eec04b201bae90f59ab42bec8d7654973dc8188
Lena <834480718@qq.com>
2021-07-30 19:16:38 +0800

    Chapter6_3rd

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.models import Page
 
 def index(request):
     category_list=Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     
     context_dict={}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
@@ -16,4 +18,14 @@ def about(request):
 
 def show_category(request, category_name_slug):
     context_dict={}
+
+    try:
+        category=Category.objects.get(slug=category_name_slug)
+        pages=Page.objects.filter(category=category)
+        context_dict['pages']=pages
+        context_dict['category']=category
+    except Category.DoesNotExist:
+        context_dict['category']=None
+        context_dict['pages']=None
+
     return render(request, 'rango/category.html', context=context_dict)
